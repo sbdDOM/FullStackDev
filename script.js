a8a3a1a517c63a0fbce97a7bbcb468dba7a2abd1
sbdDOM <81794785+sbdDOM@users.noreply.github.com>
2022-05-10 02:52:40 +0200

    DOM part 1

---
+++ script.js
@@ -0,0 +1,15 @@
+//Examine the document object
+//console.dir(document);
+
+console.log(document.forms);
+
+// Selectors
+
+/*getElementById;
+getElementsByClassName
+getElementsByTagName*/
+
+//Query Selector (grabs first one by default)
+//document.querySelector
+
+//Query Selector All (grabs multiple)
