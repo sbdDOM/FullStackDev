fa908aad1b156ba2c4905159fa415c27388f4a4a
Rivaldo Esbend <esbendrivaldo@gmail.com>
2022-05-26 23:25:47 +0200

    OMG this is going to be a journey... Async

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-//Examine the document object
-//console.dir(document);
-
-console.log(document.forms);
-
-// Selectors
-
-/*getElementById;
-getElementsByClassName
-getElementsByTagName*/
-
-//Query Selector (grabs first one by default)
-//document.querySelector
-
-//Query Selector All (grabs multiple)
+// promises and await and callback
