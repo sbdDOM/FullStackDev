6ab2cf8024808298a0ac5b5e0fc21cec5d4a4a23
Rivaldo Esbend <esbendrivaldo@gmail.com>
2022-05-27 01:25:38 +0200

    Beginners lesson: JSON

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// promises and await and callback
+//Json
+
+let companies = 
+    `[
+        {
+            "name": "Big Corpo",
+            "numberOfEmployees": 10000,
+            "ceo": "Mary",
+            "rating": 3.5
+
+        },
+        {
+            "name": "Small Corpo",
+            "numberOfEmployees": 10,
+            "ceo": null,
+            "rating": 2.0
+
+        }
+
+    ]`
+    console.log(JSON.parse(companies));
