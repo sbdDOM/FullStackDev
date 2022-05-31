2139f46a9b112983eea8459ce70beed696b33fb2
Rivaldo Esbend <esbendrivaldo@gmail.com>
2022-05-31 05:22:40 +0200

    Intermediate JS(Sort, Map, Filter, Reduce) Part 1

--- General/script.js
+++ General/script.js
@@ -134,6 +134,116 @@
      // document.getElementById('Output').innerHTML = '<h3>' + e.target.value + '</h3>';
 // }
 
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton)); 
+//_____________Arrays: forEach, filter, map, sort and reduce.______________
+const companies = [
+    {
+        "name": "Company One",
+        "category": "Finance",
+        "start": 1981,
+        "end": 2003
+    },
+    {
+        "name": "Company Two",
+        "category": "Retail",
+        "start": 1992,
+        "end": 2008
+    },
+    {
+        "name": "Company Three",
+        "category": "Auto",
+        "start": 1999,
+        "end": 2007
+    },
+    {
+        "name": "Company Four",
+        "category": "Retail",
+        "start": 1989,
+        "end": 2010
+    },
+    {
+        "name": "Company Five",
+        "category": "Technology",
+        "start": 2009,
+        "end": 2014
+    },
+    {
+        "name": "Company Six",
+        "category": "Finance",
+        "start": 1987,
+        "end": 2010
+    },
+    {
+        "name": "Company Seven",
+        "category": "Auto",
+        "start": 1986,
+        "end": 1996
+    },
+    {
+        "name": "Company Eight",
+        "category": "Technology",
+        "start": 2011,
+        "end": 2016
+    },
+    {
+        "name": "Company Nine",
+        "category": "Retail",
+        "start": 1981,
+        "end": 1989
+    }
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+//_________ForEach__________
+//----for loop----
+// for(let i=0; i<companies.length; i++){
+//     console.log(companies[i]);
+// }
+// ---forEach--- 
+// companies.forEach(function(company){
+//     console.log(company);
+// });
+
+//__________Filter____________
+//----for loop----
+// let canDrink = [];
+// for(let i=0; i<ages.length; i++){
+//     if(ages[i] >= 21){
+//         canDrink.push(ages[i]);
+//     }
+// }
+//----filter----
+// const canDrink = ages.filter(function(age){
+//     if(age >= 21){
+//         return true;
+//     }
+// });
+//----filter arrow functions----
+//const canDrink = ages.filter(age => age >= 21);
+//----filter next example----
+// const retailCompanies = companies.filter(function(company){
+//     if(company.category === 'Retail'){
+//         return true;
+//     }
+// });
+//----filter arrow functions next example----
+// const retailCompanies = companies.filter(company => (company.end - company.start >=10));
+// console.log(retailCompanies);
+
+//____________Map__________________
+//----Create array of company names---- 
+// const companyName = companies.map(function(company){
+//     return company.category;
+// });
+//----Get company name with its end and start date----
+// const companyName = companies.map(function(company){
+//     return `${company.name} [${company.start} - ${company.end}]`;
+// });
+//----Using arrow function in mapping----
+// const companyName = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
+//----Using arrow function with multiple .map functions----
+// const agesFunction = ages
+//     .map(age => Math.sqrt(age))
+//     .map(age => age * 2);
+
+//___________Sort________________
--- pyScript/utils.py
+++ pyScript/utils.py
@@ -1,4 +1,4 @@
 #using multiple py files
 
-def add_class(element, classname):
-    element.classList.add(class_name)
+# def add_class(element, classname):
+#     element.classList.add(class_name)
